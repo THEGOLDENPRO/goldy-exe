752ae982b55790ef1317d0406ba0eb12a59d27e6
THEGOLDENPRO <66202304+THEGOLDENPRO@users.noreply.github.com>
2023-08-23 19:30:08 +0100

    updates to logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,8 +14,13 @@ module.exports = {
                 goldyOrangy: {
                     DEFAULT: "#f5671b",
                     100: "#f5671b",
-                    300: "#f57d3d"
-                }
+                    300: "#f57d3d",
+                    800: "#f5be3d"
+                },
+                goldyGreen: "#d0f54c"
+            },
+            dropShadow: {
+                'flicker': "0.06rem 0 0.06rem theme(colors.goldyPink), -0.125rem 0 0.06rem theme(colors.goldyOrangy.300);"
             }
         },
 
@@ -24,15 +29,15 @@ module.exports = {
             "typewriter": ["atwriter"]
         },
         animation: {
-            "glow": "glow 3s ease-in-out infinite alternate",
+            "flicker": "flicker 0.01s infinite alternate",
         },
         keyframes: {
-            glow: {
-                "from": { 
-                    textShadow: "0 0 0px #fff, 0 0 2px #fff, 0 0 5px theme(colors.goldyOrangy.100), 0 0 10px theme(colors.goldyOrangy.100), 0 0 15px theme(colors.goldyOrangy.100), 0 0 20px theme(colors.goldyOrangy.100), 0 0 25px theme(colors.goldyOrangy.100)" 
+            flicker: {
+                "from" : {
+                    textShadow: "1px 0 0 theme(colors.goldyPink), -2px 0 0 theme(colors.goldyOrangy.300);",
                 },
-                "to": { 
-                    textShadow: "0 0 3px #fff, 0 0 5px theme(colors.goldyOrangy.300), 0 0 10px theme(colors.goldyOrangy.300), 0 0 15px theme(colors.goldyOrangy.300), 0 0 20px theme(colors.goldyOrangy.300), 0 0 25px theme(colors.goldyOrangy.300), 0 0 30px theme(colors.goldyOrangy.300)" 
+                "to" : {
+                    textShadow: "2px 0.5px 2px theme(colors.goldyPink), -1px -0.5px 2px theme(colors.goldyOrangy.300);",
                 },
             }
         }
